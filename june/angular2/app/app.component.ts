09abe790c67d5606c1256045f65bae6ef3627c98
realdubb <aleqs@gmx.com>
2016-06-25 15:46:24 -0500

    transition angular to webpack

---
+++ june/angular2/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component, enableProdMode } from '@angular/core';
+
+enableProdMode();
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Firsts Angular 2 Apps</h1>'
+})
+export class AppComponent { }
